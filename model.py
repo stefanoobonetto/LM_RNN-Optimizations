014260ba31e831d29932e5135fb4fded3071a6dd
bonnie <stefanoobonetto@gmail.com>
2024-04-17 10:49:54 +0200

    implemented BFS

--- model.py
+++ model.py
@@ -11,40 +11,16 @@ from typing import *
 
 # Define VariationalDropout class
 class VariationalDropout(nn.Module):
-    def __init__(self, dropout: float, batch_first: Optional[bool]=False):
+    def __init__(self):
         super().__init__()
-        # Initialize dropout rate and batch_first flag
-        self.dropout = dropout
-        self.batch_first = batch_first
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # If not in training mode or dropout is 0, return the input as is
-        if not self.training or self.dropout <= 0.:
+    def forward(self, x, dropout=0.5):
+        if not self.training or not dropout:
             return x
-
-        # Check if input is a PackedSequence
-        is_packed = isinstance(x, PackedSequence)
-        if is_packed:
-            # If it is, unpack it
-            x, batch_sizes = x
-            max_batch_size = int(batch_sizes[0])
-        else:
-            # If it's not, set batch_sizes to None and get the batch size from x
-            batch_sizes = None
-            max_batch_size = x.size(0)
-
-        # Apply the same dropout mask across the entire sequence
-        if self.batch_first:
-            m = x.new_empty(max_batch_size, 1, x.size(2), requires_grad=False).bernoulli_(1 - self.dropout)
-        else:
-            m = x.new_empty(1, max_batch_size, x.size(2), requires_grad=False).bernoulli_(1 - self.dropout)
-        x = x.masked_fill(m == 0, 0) / (1 - self.dropout)
-
-        # If input was a PackedSequence, return it as such
-        if is_packed:
-            return PackedSequence(x, batch_sizes)
-        else:
-            return x
+        m = x.data.new(1, x.size(1), x.size(2)).bernoulli_(1 - dropout)
+        mask = torch.tensor(m, requires_grad=False) / (1 - dropout)
+        mask = mask.expand_as(x)
+        return mask * x
 
 # Define LM_LSTM class
 class LM_LSTM(nn.Module):
@@ -56,7 +32,7 @@ class LM_LSTM(nn.Module):
         self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
         # If variational dropout is enabled in the config, initialize VariationalDropout
         if config.var_drop:
-            self.input_drop = VariationalDropout(emb_dropout, batch_first=batch_first)
+            self.input_drop = VariationalDropout()
         
         # Define LSTM layer
         self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)
@@ -65,7 +41,7 @@ class LM_LSTM(nn.Module):
         self.output = nn.Linear(hidden_size, output_size)
         # If variational dropout is enabled in the config, initialize VariationalDropout for output
         if config.var_drop:
-            self.output_drop = VariationalDropout(out_dropout, batch_first=batch_first)
+            self.output_drop = VariationalDropout()
         
         # Tie the weights of the output and embedding layers
         self.output.weight = self.embedding.weight
